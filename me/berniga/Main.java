e05daf3c124caffba54ef650685f518a30e57b6d
BernigaChristian <christian.berniga@itisondrio.org>
2021-12-06 18:41:15 +0100

    up

--- me/berniga/Main.java
+++ me/berniga/Main.java
@@ -1,5 +1,6 @@
 package me.berniga;
-import java.util.Random;
+import java.io.PrintWriter;
+import java.util.Scanner;
 /**
  * @author : Christian Berniga
  * @class : 4 D
@@ -9,60 +10,35 @@ import java.util.Random;
  **/
 public class Main {
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
         Scaffale s1=new Scaffale();
+        String choice;
         System.out.println("S1:\nNSHELVES: "+Scaffale.getNumRipiani()+"\tNMAXBOOKS: "+s1.getNumMaxLibri()+"\tNBOOKS: "+s1.getNumLibri());
-        /*
-        NO EXCEPTIONS THROWN
-        */
-        System.out.println("---------------------------------------\nNO EXCEPTIONS THROWN");
-        try{s1.setLibro(new Libro("FirstTitle","FirstAuthor",new Random().nextInt(1000)+100),1,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(PosizioneNonVuota e1){System.out.println(e1.toString());}
-
-        try{System.out.println(s1.getLibro(1,0).toString()+"\nNBOOKS SHELF 1: "+s1.getNumLibri(1));}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(NullPointerException e1){System.out.println("NullPointerException unhandled");}   //xche nel getLibro si può verificare anche una NullPointerException
-
-        try{s1.rimuoviLibro(1,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-
-        /*
-        POSIZIONENONVALIDA EXCEPTIONS THROWN
-        */
-        System.out.println("---------------------------------------\nPOSIZIONENONVALIDA EXCEPTIONS THROWN");
-        try{s1.setLibro(new Libro("SecondTitle","SecondAuthor",new Random().nextInt(1000)+100),-1,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(PosizioneNonVuota e1){System.out.println(e1.toString());}
-
-        try{ System.out.println(s1.getLibro(-1,0).toString());}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(NullPointerException e1){System.out.println("NullPointerException unhandled");}
-
-        try{s1.rimuoviLibro(100,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-
-        /*
-        POSIZIONENONVUOTA EXCEPTIONS THROWN
-        */
-        System.out.println("---------------------------------------\nPOSIZIONENONVUOTA EXCEPTIONS THROWN");
-        try{s1.setLibro(new Libro("ThirdTitle","ThirdAuthor",new Random().nextInt(1000)+100),1,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(PosizioneNonVuota e1){System.out.println(e1.toString());}
-
-        try{s1.setLibro(new Libro("FourthTitle","FourthAuthor",new Random().nextInt(1000)+100),1,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(PosizioneNonVuota e1){System.out.println(e1.toString());}
-
-        /*
-        NULLPOINTEREXCEPTION EXCEPTIONS THROWN
-        */
-        System.out.println("---------------------------------------\nNULLPOINTEREXCEPTION EXCEPTIONS THROWN");
-        try{s1.rimuoviLibro(1,0);}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-
-        try{ System.out.println(s1.getLibro(1,0).toString());}
-        catch(PosizioneNonValida e){System.out.println(e.toString());}
-        catch(NullPointerException e){System.out.println("NullPointerException unhandled");}
-
+        System.out.print("-----------------------------------------\nType your choice: ");
+        while(!(choice= scan.nextLine()).equalsIgnoreCase("exit")){
+            try{
+                switch(choice){
+                    case "set":
+                        System.out.println("Type the TITLE ,the AUTHOR,PAGES,the SHELF and the POSITION,separated by a comma");
+                        String[] temp=scan.nextLine().split(",");
+                        s1.setLibro(new Libro(temp[0],temp[1],Integer.parseInt(temp[2])),Integer.parseInt(temp[3]),Integer.parseInt(temp[4]));
+                        break;
+                    case "remove": case "get":
+                        System.out.println("  Type the shelf: ");
+                        int shelf= scan.nextInt();
+                        System.out.println("  Type the position: ");
+                        int position= scan.nextInt();
+                        scan.nextLine();        //se nn si mettesse il successico scan di CHOICHE prende la parte rimanente della linea dopo i 2 nextInt
+                        if(choice.equalsIgnoreCase("remove"))
+                            s1.rimuoviLibro(shelf,position);
+                        else
+                            System.out.println(s1.getLibro(shelf,position).toString());
+                        break;
+                    default: System.out.println("Invalid choice");
+                }
+            }
+            catch(PosizioneNonValida | PosizioneNonVuota | NullPointerException e ){System.out.println(e.toString());}
+            System.out.print("-----------------------------------------\nType your choice: ");
+        }
     }
 }
